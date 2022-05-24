5e6ccff8f13c6498c7b48c42e8684327ac11cb00
Ihimbazwe Elissa <ihimbazwelisa@gmail.com>
2022-05-24 20:54:41 +0200

    fixed footer links

--- src/components/SocialMediaLinks/SocialMediaLinks.js
+++ src/components/SocialMediaLinks/SocialMediaLinks.js
@@ -1,12 +1,11 @@
-import './SocialMediaLinks.css';
 
 const SocialMediaLinks = () => {
     return (
-        <div className="social-media-links">
-            <a href='/facebook'>FaceBook</a>
-            <a href='/instagram'>Instagram</a>
-            <a href='/instagram'>YouTube</a>
-            <a href='/instagram'>LinkedIn</a>
+        <div className="w-4/5 mx-auto my-60 text-center">
+            <a className='px-2' href='/facebook'>FaceBook</a>
+            <a className='px-2' href='/instagram'>Instagram</a>
+            <a className='px-2' href='/instagram'>YouTube</a>
+            <a className='px-2' href='/instagram'>LinkedIn</a>
         </div>
     );
 }
